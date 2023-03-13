36c6c6fc6cece48bcbbcff5664d1d0f8207af6b3
Ahmed Rahi <54910341+arahi7860@users.noreply.github.com>
2023-03-13 16:28:20 -0400

    changes

--- ux-sei/src/App.js
+++ ux-sei/src/App.js
@@ -1,12 +1,13 @@
-
-import './App.css';
-import Navbar from './components/NavBar';
+import "./App.css";
+import Navbar from "./components/NavBar";
+import Carousel from "./components/carousel";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-    </div >
+      <Carousel />
+    </div>
   );
 }
 
---
+++ ux-sei/src/components/Carousel.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Carousel() {
+  return <div>Hello World!</div>;
+}
+
+// Get the four images:
+// New Arrivals (New Arrivals.avif)
+// Popular Blends (Popular Blends.avif)
+// Find Nearest Cafe (Find Locations.avif)
+// All Products (All Products.png)
