0399da6581dedb667c4f6e3a94b5c3c710f26104
Ahmed Rahi <54910341+arahi7860@users.noreply.github.com>
2023-03-13 22:58:48 -0400

    added transitioning to Carousel.jsx

--- ux-sei/src/components/Carousel.jsx
+++ ux-sei/src/components/Carousel.jsx
@@ -1,8 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "../App.css";
+import picOne from "/Users/ahmedrahi/dev/unit-3/UX-SEI/ux-sei/src/Img/New Arrivals.avif";
+import picTwo from "/Users/ahmedrahi/dev/unit-3/UX-SEI/ux-sei/src/Img/Popular Blends.avif";
+import picThree from "/Users/ahmedrahi/dev/unit-3/UX-SEI/ux-sei/src/Img/Find Locations.avif";
+import picFour from "/Users/ahmedrahi/dev/unit-3/UX-SEI/ux-sei/src/Img/All Products.png";
 
-export default function Carousel() {
-  return <div>Hello World!</div>;
-}
+const images = [picOne, picTwo, picThree, picFour];
+
+const Carousel = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleIndicatorClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="carousel">
+      <div className="slides">
+        {images.map((image, index) => (
+          <img
+            key={index}
+            className={index === activeIndex ? "active" : ""}
+            src={image}
+            alt={`image-${index}`}
+            onClick={() => setActiveIndex((prevIndex) => prevIndex + 1)}
+            onTransitionEnd={() =>
+              setActiveIndex((prevIndex) =>
+                prevIndex === images.length - 1 ? 0 : prevIndex + 1
+              )
+            }
+          />
+        ))}
+      </div>
+      <div className="indicators">
+        {images.map((_, index) => (
+          <div
+            key={index}
+            className={index === activeIndex ? "active" : ""}
+            onClick={() => handleIndicatorClick(index)}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
 
 // Get the four images:
 // New Arrivals (New Arrivals.avif)
