9f27b3d90dbb2393044284c9f91704603036c4bd
Joynae Whitehurst <Joynaew@gmail.com>
2023-03-14 15:35:38 -0400

    darm mode

--- ux-sei/src/components/Footer.jsx
+++ ux-sei/src/components/Footer.jsx
@@ -1,22 +1,22 @@
 import Facebook from '../Img/Facebook.png'
 import Instagram from '../Img/Instagram.png'
 import Twitter from '../Img/Twitter.png'
+import './NavBar.css'
 
-
-export function Footer() {
+export default function Footer() {
   return (
-    <>
-      <p>The Druncken Cup</p>
+    <div className='footer'>
+      <p className='name'>The Druncken Cup</p>
       <p>10 E 21st St #2, New York, NY</p>
       <p>10010</p>
       <p>Sitemap</p>
       <p>Privacy</p>
       <p>Careers</p>
       <p>Join Our Socials</p>
-      <img src={Facebook} alt='social' />
-      <img src={Twitter} alt='social' />
-      <img src={Instagram} alt='social' className='pic' />
-    </>
+      <img src={Facebook} alt='social' className="icon" />
+      <img src={Twitter} alt='social' className="icon" />
+      <img src={Instagram} alt='social' className='icon' />
+    </div>
 
   )
 }
